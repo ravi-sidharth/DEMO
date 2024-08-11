46ce8a87d14a9b8360137cf159ddb1b5d12ec7b1
ravi <thenewravy@gmail.com>
2024-08-11 18:52:06 +0530

    day 1

---
+++ variableAndDataTypes.js
@@ -0,0 +1,45 @@
+// Day 1: Variable and Data Types 
+
+//Task 1 :
+var number =5
+console.log(number)
+
+
+//Task 2: 
+let name= "Ravi"
+console.log(name)
+
+//Task 3:
+const boolean = false
+console.log(boolean)
+
+//Task 4:
+const student ={ 
+	name:"Ravi",
+	class:12,
+	rollNo:22
+	}
+
+const array = [1,2,3,4,5,6]
+
+console.log(typeof(number))
+console.log(typeof(name))
+console.log(typeof(boolean));
+console.log(typeof(student));
+console.log(typeof(array))
+
+
+//Task 5:
+let a=5
+a=10
+console.log(num)
+
+
+//Task 6:
+const num=5
+// num=6	// in the const operation we can't assign any value we can only assign the value let and var key.
+console.log(num)
+
+
+
+//Task 6: 
